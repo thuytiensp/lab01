fe337abeec919407b9460dde19c50407e2fdf89e
tien tran <thuytienspvt@gmail.com>
2017-10-17 09:46:03 +0700

    Create some view, route for root

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>My restaurant</title>
+</head>
+<body>
+	@include('partials.header')
+	@yield('content')
+	@include('partials.footer')
+</body>
+</html>
